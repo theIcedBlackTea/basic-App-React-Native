8dd34369a550462ffaf449bcfd241931281c7dd9
theIcedBlackTea <blacktemplehood@gmail.com>
2025-03-07 17:29:54 -0600

    creacion de proyecto con react-native y expo

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [numberOne, setNumberOne] = useState("");
+  const [numberTwo, setNumberTwo] = useState("");
+  const [result, setResult] = useState("");
+  const handleBtnSum=()=>{
+    setResult(parseInt(numberOne)+parseInt(numberTwo))
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>S U M A   D E   D O S   N Ú M E R O S</Text>
+      <TextInput
+        value={numberOne}
+        onChangeText={setNumberOne}
+        style={styles.input}
+        keyboardType='numeric'
+        placeholder='Teclea el primer valor '
+      />
+      <TextInput
+        value={numberTwo}
+        onChangeText={setNumberTwo}
+        style={styles.input}
+        keyboardType='numeric'
+        placeholder='Teclea el segundo valor '
+        />
+      <Button
+        title="Sumar"
+        onPress={handleBtnSum}
+        />
+      <Text style={styles.title}>Resultado: {result}</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -16,5 +42,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
+  title:{
+    fontWeight: 'bold',
+    marginBottom: 30
+  },
+  input:{
+    width:'100%',
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: '#efefef',
+    marginBottom: 10
+  },
+  btn:{
+
+  }
 });
